6b3fd2cedd0550b21f61705e7d08671bb479298e
Dzmitry Dzemidziuk <extern.dzemidziuk_dzmitry@allianz.com>
2024-09-29 20:10:45 +0200

    open the app on full screen

--- composeApp/src/desktopMain/kotlin/org/yournews/main.kt
+++ composeApp/src/desktopMain/kotlin/org/yournews/main.kt
@@ -1,14 +1,32 @@
 package org.yournews
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import org.yournews.ui.MainScreen
+import java.awt.GraphicsEnvironment
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "YourNews",
-    ) {
-        MainScreen()
-    }
+    val graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment()
+    val screenDevices = graphicsEnvironment.screenDevices
+
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "YourNews",
+            state = rememberWindowState(
+                position = WindowPosition(
+                    screenDevices[0].defaultConfiguration.bounds.x.dp,
+                    screenDevices[0].defaultConfiguration.bounds.y.dp
+                ),
+                size = DpSize(
+                    screenDevices[0].defaultConfiguration.bounds.width.dp,
+                    screenDevices[0].defaultConfiguration.bounds.height.dp
+                )
+            )
+        ) {
+            MainScreen()
+        }
 }
